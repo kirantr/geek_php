4992bb45c0a005aa50dca0887e908ecd59d7e2ed
kirantr <kirantr@yandex.ru>
2017-08-04 17:41:29 +0300

    Update config.php

--- task1/config.php
+++ task1/config.php
@@ -1,6 +1,6 @@
 <?php
 
-  $upload_path="/usr/home/user7/public_html/MYPHP/geek_php/task1/files/";
+  $uploadPath="/usr/home/user7/public_html/MYPHP/geek_php/task1/files/";
 //  $uploadPath = "/var/www/html/geek_php/task1/files/";
   
 ?>
