f91a0f3448bf26e5c87bfc09454cc97c9bdffdf0
kirantr <kirantr@yandex.ru>
2017-08-14 16:03:26 +0300

    14.08 task3

--- task3/libs/FileRead.php
+++ task3/libs/FileRead.php
@@ -1,82 +1,88 @@
 <?php
 
-  class FileRead
-  {
+ class FileRead
+ {
 
-		public function __construct($pathFile)
-		{
-			 if (file_exists($pathFile))
-			 {
-				  $this->readFile = file($pathFile);
-				  return($this->readFile);
-			 }
-		}
+     public function __construct($pathFile)
+     {
+         if (file_exists($pathFile))
+         {
+             $this->readFile = file($pathFile);
+             return($this->readFile);
+         }
+     }
 
-		public function fileGetCont()
-		{
-			 $content = file_get_contents($this->filePath);
-			 return $content;
-		}
+     public function fileGetCont()
+     {
+         if (!empty($this->readFile))
+        {
+         $content = file_get_contents($this->readFile);
+         return $content;
+        }
+        else
+        {
+            echo  NOT_FILE;
+        }
+     }
 
-		public function readArrayStr($inputStr, $flagStr, $replaceStr)
-		{
-			 foreach ($this->readFile as $key => $stringValue)
-			 {
-				  if (is_numeric($inputStr))
-				  {
-						if ($key == $inputStr)
-						{
-							 if ($flagStr == 'read')
-							 {
-								  return $key . ' ' . $stringValue;
-							 } elseif ($flagStr == 'replace')
-							 {
-								  return $this->readFile [$inputStr] = $key . ' ' . $replaceStr;
-							 }
-							 {
-								  //123 
-							 }
-						}
-				  } else
-				  {
-						return NOT_NUM;
-				  }
-			 }
-		}
+     public function readArrayStr($inputStr, $flagStr, $replaceStr)
+     {
+         foreach ($this->readFile as $key => $stringValue)
+         {
+             if (is_numeric($inputStr))
+             {
+                 if ($key == $inputStr)
+                 {
+                     if ($flagStr == 'read')
+                     {
+                         return $key . ' ' . $stringValue;
+                     } elseif ($flagStr == 'replace')
+                     {
+                         return $this->readFile [$inputStr] = $key . ' ' . $replaceStr;
+                     } {
+                         //123 
+                     }
+                 }
+             } else
+             {
+                 return NOT_NUM;
+             }
+         }
+     }
 
-		public function readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb)
-		{
-			 foreach ($this->readFile as $key => $stringValue)
-			 {
-				  if (is_numeric($inputStr))
-				  {
-						if ($key == $inputStr)
-						{
-							 if ($inputSymb <= strlen($stringValue))
-							 {
-								  if ($flagSymb == 'read')
-								  {
-										return $inputSymb . ' ' . $stringValue{$inputSymb};
-								  } elseif ($flagSymb == 'replace')
-								  {
-										return $this->readFile[$inputStr]{$inputSymb} = $repSymb;
-								  }
-							 }
-						}
-				  } else
-				  {
-						return NOT_NUM;
-				  }
-			 }
-		}
+     public function readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb)
+     {
+         foreach ($this->readFile as $key => $stringValue)
+         {
+             if (is_numeric($inputStr))
+             {
+                 if ($key == $inputStr)
+                 {
+                     if ($inputSymb <= strlen($stringValue))
+                     {
+                         if ($flagSymb == 'read')
+                         {
+                             return $inputSymb . ' ' . $stringValue{$inputSymb};
+                         } elseif ($flagSymb == 'replace')
+                         {
+                             return $this->readFile[$inputStr]{$inputSymb} = $repSymb;
+                         }
+                     }
+                 }
+             } else
+             {
+                 return NOT_NUM;
+             }
+         }
+     }
 
-		public function saveContent($cont, $pathFileModif, $repStr, $repSymb)
-		{
-			 if (isset($repStr) || isset($repSymb))
-			 {
-				  file_put_contents($pathFileModif, $cont);
-			 }
-		}
+     public function saveContent($cont, $pathFileModif, $repStr, $repSymb)
+     {
+         if (isset($repStr) || isset($repSymb))
+         {
+             file_put_contents($pathFileModif, $cont);
+         }
+     }
 
-  }
-  
+ }
+ 
--- task3/templates/index.php
+++ task3/templates/index.php
@@ -2,54 +2,61 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/style.css" rel="stylesheet">
         <title>Task3</title>
     </head>
     <body>
-		  <?php
-			 echo'<form  method="post"> 
-						  Number string for output:
-						 <input type="text" name="inputStr"><br>
-						  Number symbol for output:
-						  <input type="text" name="inputSymb"><br> 
-						  Number string for replace:
-						 <input type="text" name="repStr"><br>
-						  Number symbol for replace:
-						  <input type="text" name="repSymb"><br> 
-						 <input type=submit value="Show"> 
-					</form>';
-			 echo $objFileRead -> fileGetCont();
-			 
-			 $inputStr = ($_POST['inputStr']);
-			 $repStr = ($_POST['repStr']);
-			 $inputSymb = ($_POST['inputSymb']);
-			 $repSymb = ($_POST['repSymb']);
+        <div class="container center-block ">
+            <h1>String/Symbol chenger</h1>
+            <form  class="form-inline" method="post"> 
+                Number string for output:
+                <input class="input-sm"type="text" name="inputStr"><br>
+                Number symbol for output:
+                <input class="input-sm" type="text" name="inputSymb"><br> 
+                Number string for replace:
+                <input class="input-sm" type="text" name="repStr"><br>
+                Number symbol for replace:
+                <input class="input-sm" type="text" name="repSymb"><br> 
+                <input type=submit value="Show"> 
+            </form>
 
-			 foreach ($objFileRead->readFile as $key => $stringValue)
-			 {
-				  echo "$stringValue" . '<br>';
-			 }
-			 $flagStr = 'read';
-			 $replaceStr = '';
-			 $cont1 = $objFileRead->readArrayStr($inputStr, $flagStr, $replaceStr);
-			 echo '<h3> String:</h3>' . $cont1;
-			 $flagSymb = 'read';
-			 $cont2 = $objFileRead->readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb);
-			 echo '<h3> Symbol:</h3>' . $cont2;
+            <?php
+             echo '<h3>File contents:</h3>'
+             . $objFileRead->fileGetCont();
 
-			 $flagStr = 'replace';
-			 $replaceStr = 'This is a modified string';
-			 $inputStr = $repStr;
-			 $cont3 = $objFileRead->readArrayStr($inputStr, $flagStr, $replaceStr);
-			 echo '<h3> Modified String:</h3>' . $cont3;
-			 
-			 $flagSymb = 'replace';
-			 $repSymb = 'M';
-			 $inputSymb = $repSymb;
-			 $cont4 = $objFileRead->readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb);
-			 echo '<h3> Modified symbol:</h3>' . $cont4;
-			 
-			 $cont=$cont1 . $cont2 . $cont3 . $cont4;
-			 $objFileRead->saveContent($cont, $pathFileModif, $repStr, $repSymb);
-		  ?>
+             $inputStr = ($_POST['inputStr']);
+             $repStr = ($_POST['repStr']);
+             $inputSymb = ($_POST['inputSymb']);
+             $repSymb = ($_POST['repSymb']);
+
+             foreach ($objFileRead->readFile as $key => $stringValue)
+             {
+                 echo "$stringValue" . '<br>';
+             }
+             $flagStr = 'read';
+             $replaceStr = '';
+             $cont1 = $objFileRead->readArrayStr($inputStr, $flagStr, $replaceStr);
+             echo '<h3> String:</h3>' . $cont1;
+             $flagSymb = 'read';
+             $cont2 = $objFileRead->readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb);
+             echo '<h3> Symbol:</h3>' . $cont2;
+
+             $flagStr = 'replace';
+             $replaceStr = 'This is a modified string';
+             $inputStr = $repStr;
+             $cont3 = $objFileRead->readArrayStr($inputStr, $flagStr, $replaceStr);
+             echo '<h3> Modified String:</h3>' . $cont3;
+
+             $flagSymb = 'replace';
+             $repSymb = 'M';
+             $inputSymb = $repSymb;
+             $cont4 = $objFileRead->readArraySymb($inputStr, $inputSymb, $flagSymb, $repSymb);
+             echo '<h3> Modified symbol:</h3>' . $cont4;
+
+             $cont = $cont1 . $cont2 . $cont3 . $cont4;
+             $objFileRead->saveContent($cont, $pathFileModif, $repStr, $repSymb);
+            ?>
+        </div>
     </body>
 </html>
