9bd469f773dfef7b3ee372fec4af26159cb90ebd
kirantr <kirantr@yandex.ru>
2017-08-14 10:10:55 +0300

    test

--- task4/libs/Sql.php
+++ task4/libs/Sql.php
@@ -1,64 +1,69 @@
 <?php
 
-  class Sql
-  {
+class Sql
+{
 
-		private $select;
-		private $from;
-		private $where;
+    private $select;
+    private $from;
+    private $where;
 
-		public function select($select)
-		{
-			 $this->select = 'SELECT' . $select;
-			 return $this;
-		}
+    public function select($select)
+    {
+        $this->select = 'SELECT' . $select;
+        return $this;
+    }
 
-		public function from($table)
-		{
-			 $this->name = 'FROM' . $table;
-			 return $this;
-		}
+    public function from($table)
+    {
+        $this->name = 'FROM' . $table;
+        return $this;
+    }
 
-		public function where($where, $val)
-		{
-			 if ($tableName == PG_TB_NAME)
-			 {
-				  $key = "key";
-			 } else
-			 {
-				  $key = "`key`";
-			 }
-			 $this->where = " WHERE $key=" . "'" . $val . "'";
-			 return $this;
-		}
+    public function where($where, $val)
+    {
+        if ($tableName == PG_TB_NAME)
+        {
+            $key = "key";
+        } else
+        {
+            $key = "`key`";
+        }
+        $this->where = " WHERE $key=" . "'" . $val . "'";
+        return $this;
+    }
 
-		public function insert($table)
-		{
-			 if ($table == PG_TB_NAME)
-			 {
-				  $keyData = '(key, data)';
-			 } 
-			 else
-			 {
-				  $keyData = '(`key`, `data`)';
-			 }
-			 $this->insert = "INSERT INTO " . $table . $keyData;
-		}
+    public function insert($table)
+    {
+        if ($table == PG_TB_NAME)
+        {
+            $keyData = '(key, data)';
+        } else
+        {
+            $keyData = '(`key`, `data`)';
+        }
+        $this->insert = "INSERT INTO " . $table . $keyData;
+    }
 
-		public function exec()
-		{
-			 if (
-				!empty($this->select) && !empty($this->from) && !empty($this->where)
-			 )
-			 {
-				  $this->query = $this->select . $this->from . $this->where;
-				  return $this->query;
-			 } else
-			 {
+        public function values($key, $data)
+    {
+        $this->values = " VALUES ('".$key."', '".$data."')";
+        return $this;
+    }
+
+    public function exec()
+    {
+        if (
+                !empty($this->select) && !empty($this->from) && !empty($this->where)
+        )
+        {
+            $this->query = $this->select . $this->from . $this->where;
+            return $this->query;
+        } else
+        {
 //				  return NO_PROPERTIES;
-			 }
-		}
+        }
+    }
 
-  }
+}
 
 ?>
