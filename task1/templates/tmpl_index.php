7bd8e9051e18c46d5ece089f16f0a1bb7fe04fe0
kirantr <kirantr@yandex.ru>
2017-08-21 10:46:25 +0300

    task1 finish

--- task1/templates/tmpl_index.php
+++ task1/templates/tmpl_index.php
@@ -3,39 +3,47 @@
     <head>
         <meta charset="utf-8">
 
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/style.css" rel="stylesheet">
+
         <title>Task1</title>
     </head>
     <body>
-        <form method=post action="./index.php" enctype=multipart/form-data> 
-            Select file: <input type=file name=UserFile> 
-            <input type=submit value=Upload> 
-        </form>
-<br><table class="table" border="1">
-            <thead>
-                <tr>
-                <th>N</th>
-                <th>Files</th>
-                <th>Size</th>
-                <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php
-			 echo "$report";
-			 echo "$reportExists";
+        <div class="container center-block">
+            <div class="col-md-offset-3 col-md-6">
+                <h2>Uploader</h2><br>
+                <form method=post action="./index.php" enctype=multipart/form-data> 
+                    Select file: <input type=file name=UserFile> 
+                    <input type=submit value=Upload> 
+                </form><br>
+                <table class="table" border="1">
+                    <thead>
+                        <tr>
+                            <th >N</th>
+                            <th>Files</th>
+                            <th>Size</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                         echo "$report";
+                         echo "$reportExists";
 
-         foreach ($outArray as $key => $value)
-         {
-             echo '<tr>'
-             . '<td>' . $value['num'] . '</td>' .
-                '<td>' . $value['fileName'] . '</td>' .
-                '<td>' . $value['byte'] . '</td>' .
-                '<td>' . $value['del'] . '</td>'
-             . '</tr>'
-             ;
-         }
-
-        ?>
-				</tbody>
+                         foreach ($outArray as $key => $value)
+                         {
+                             echo '<tr>'
+                             . '<td>' . $value['num'] . '</td>' .
+                             '<td>' . $value['fileName'] . '</td>' .
+                             '<td>' . $value['byte'] . '</td>' .
+                             '<td>' . $value['del'] . '</td>'
+                             . '</tr>'
+                             ;
+                         }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
