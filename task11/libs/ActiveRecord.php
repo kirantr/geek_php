8e25b0788317bcfeb39e431ab481b7a21a36d0cb
kirantr <kirantr@yandex.ru>
2017-09-15 15:05:09 +0300

    Update ActiveRecord.php

--- task11/libs/ActiveRecord.php
+++ task11/libs/ActiveRecord.php
@@ -1,39 +1,54 @@
 <?php
 
-class ActiveRecord extends SQL
-{
+ class ActiveRecord extends SQL
+ {
 
-    protected $connectProp;
-    protected $stack = array();
-    protected $dataRow = array();
+     protected $connectProp;
+     protected $stack = array();
+     protected $dataRow = array();
+//     public $n;
 
-    public function __set($name, $value)
-    {
-        if (array_key_exists($name, $this->stack))
-        {
-            $this->stack[$name] = $value;
-            return true;
-        }
-    }
+                public function __set($name, $value)
+             {
+                 echo "Установка '$name' в '$value'\n";
+//                 $this->data = $value;
+             }
 
-    public function __get($name)
-    {
-        if (array_key_exists($name, $this->stack))
-        {
-            return $this->stack[$name];
-        }
-    }
+             public function __get($name)
+             {
+                 echo "Получение '$name'\n";
+//                 if (array_key_exists($name, 
+//                 $this->data;
+//                     ))
+                 {
+//                     return $this->data[$name];
+                 }
+             }
 
-    public function save()
-    {
+//         }
+
+     public function save()
+     {
 //INSERT
          $objSQL->insert($nameTable, $key)->values('user7', $_POST['text'])->exec();
-             $result = mysqli_query($this->dbServer, $query);
+         $result = mysqli_query($this->dbServer, $query);
 
 //UPDATE
          $objSQL->update($nameTable)->set($data, $_POST['text'])
              ->where('user7', $key)->exec();
-        
-    }
+     }
 
-}
+     public function getSelect()
+     {
+//SELECT
+//         $query = "SELECT " . $this->data . " FROM " . NAME_TABLE;
+////         $result = mysqli_query($this->dbServer, $query);
+//         while ($row = mysqli_fetch_assoc($result))
+//         {
+//             $this->row[] = $row;
+//         }
+//         return $this->row;
+     }
+
+ }
+ 
