e0d8edff011cf7abfa2ae4cbdc621138dd1d943f
kirantr <kirantr@yandex.ru>
2017-09-15 15:06:59 +0300

    Update index.php

--- task11/index.php
+++ task11/index.php
@@ -2,13 +2,14 @@
 
  include_once './config.php';
  include_once 'libs/Sql.php';
- include_once 'libs/MySql.php';
- include_once 'libs/PostgreSQL.php';
+ include_once 'libs/MyTest.php';
+ include_once 'libs/ActiveRecord.php';
 //MYSQL
- $objMySQL = new MySQL();
- $objPgSQL = new PostgreSQL();
+ $objMySQL = new MyTest();
+ $objAR = new ActiveRecord();
 
  include_once './templates/tmpl_index.php';
+//               echo $objAR->getSelect();
 
  if (isset($_POST['flag']))
  {
