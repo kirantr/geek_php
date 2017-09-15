5889ccd025b552d83511dc0acfc62927e4714562
kirantr <kirantr@yandex.ru>
2017-09-15 15:04:18 +0300

    Update tmpl_index.php

--- task11/templates/tmpl_index.php
+++ task11/templates/tmpl_index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +6,7 @@
         <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="css/style.css" rel="stylesheet">
 
-        <title></title>
+        <title>MySQL & PostgreSQL</title>
     </head>
     <body>
         <div class="col-md-offset-4 col-md-7">
@@ -16,10 +17,6 @@
                 <div class="col-md-offset-4 col-md-7">
                     <p><input type="text" name="text" value="Input Data"> </p>
                 </div>
-                <div class="col-md-offset-4 col-md-8">
-                    <p><input type="radio" name="db" value="mysql" checked> MySQL
-                        <span class="col-md-offset-2">  <input type="radio" name="db" value="pg"> PostgreSQL</p></span>
-                    </div>
                 <div class="col-md-offset-3 col-md-9">
                     <br><p><input type="radio" name="flag" value="select" checked> Select</p>
                     <p><input type="radio" name="flag" value="insert"> Insert</p>
@@ -31,20 +28,27 @@
                 </div>
             </form>
             <?php
+            if (
+                    isset($_POST['db']) || isset($_POST['flag'])
+            )
+            {
 //SELECT
-             if ($_POST['flag'] == 'select')
-             {
-                 $selectMySQL = $objMySQL->select("`key`, `data`")->
-                         from(NAME_TABLE)->where('user7', "`key`")->exec();
-                 foreach ($selectMySQL as $value)
-                 {
-                     echo
-                     '<div class="col-md-offset-4 col-md-4 output">'
-                     . $value['key'] . ' ' . $value['data']
-                     . "</div>";
-                 }
-             }
-
+                if ($_POST['flag'] == 'select')
+                {
+//                    $selectMySQL = $objMySQL->select("`key`, `data`")->
+//                                    from(NAME_TABLE)->where('user7', "`key`")->exec();
+//                    foreach ($selectMySQL as $value)
+//                    {
+//                        echo
+//                        '<div class="col-md-offset-4 col-md-4 output">'
+//                        . $value['key'] . ' ' . $value['data']
+//                        . "</div>";
+//                    }
+                }
+            }
+// $objAR = new ActiveRecord();
+                $objAR->a = 1;
+//               echo $objAR->getSelect();
             ?>
         </div>
     </body>
