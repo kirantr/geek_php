fa61684c4845f8e8dfb5b6b5397ee25210af6766
kirantr <kirantr@yandex.ru>
2017-09-17 11:14:53 +0300

    17.09 test1

--- task11/test.php
+++ task11/test.php
@@ -8,7 +8,7 @@
     <body>
         <?php
 
-        $link = new mysqli("localhost", "user1", "tuser1", "world");
+        $link = new mysqli("localhost", "user1", "tuser1", "user1");
 /* проверка соединения */
 if (mysqli_connect_errno()) {
     printf("Не удалось подключиться: %s\n", $mysqli->connect_error);
@@ -25,8 +25,13 @@ else
 }
 
 /* Select запросы возвращают результирующий набор */
-if ($result = mysqli_query($link, "SELECT * FROM City LIMIT 10")) {
+if ($result = mysqli_query($link, "SELECT `key`, `data` FROM MY_TEST ")) {
     printf("Select вернул %d строк.\n", mysqli_num_rows($result));
+	 
+	 /* Выборка результатов запроса */
+    while( $row = mysqli_fetch_assoc($result) ){
+        echo("<br>". $row['key']. $row['data']);
+    } 
 
     /* очищаем результирующий набор */
     mysqli_free_result($result);
